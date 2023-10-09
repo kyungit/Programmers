a4cabbae1b2c23408716331a8446a23cb0a675be
kyungit <144915124+kyungit@users.noreply.github.com>
2023-10-10 00:15:38 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.24 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.replaceAll("[^0-9]","").split("");
+        
+        for(String s : arr){
+            answer += Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
