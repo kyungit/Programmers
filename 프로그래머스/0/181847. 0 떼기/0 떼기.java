ca56797acb1454abdd593cecb4ee8234ec51a1ea
kyungit <144915124+kyungit@users.noreply.github.com>
2024-05-28 20:50:07 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int num = Integer.parseInt(n_str);
+        answer = Integer.toString(num);
+        
+        return answer;
+    }
+}
